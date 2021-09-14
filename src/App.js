68ccd163cd3ae1ee7c1a381a66ab15f24cb55913
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-09-14 02:37:57 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { render } from "@testing-library/react";
+import Home from "./pages/Home/Home";
+
 function App() {
-  //prueba
-  return null;
+  return (<Home />);
 }
+
 
 export default App;
